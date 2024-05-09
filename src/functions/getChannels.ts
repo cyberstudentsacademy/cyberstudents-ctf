447dc4a8c33426c15b553801f7bd8ba8f57d5c2b
GodderE2D <55918888+GodderE2D@users.noreply.github.com>
2024-05-08 21:03:56 -0400

    fix(getChannels): allow announcement channels

--- src/functions/getChannels.ts
+++ src/functions/getChannels.ts
@@ -4,7 +4,7 @@ import { config, env } from "../index.js";
 
 export function getChallengeChannel(client: Client) {
   const channel = client.channels.cache.get(config.challengeChannelId);
-  if (channel?.type !== ChannelType.GuildText)
+  if (!channel || ![ChannelType.GuildText, ChannelType.GuildAnnouncement].includes(channel.type))
     throw new Error("Challenge channel cannot be found or is not a text channel");
 
   return channel;
